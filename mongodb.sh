4e1a869b75d63a7312d39e450a2d5ab7edb294ae
sahityareddy <sahitya.murikinati@insparx.com>
2023-05-29 16:12:40 +0200

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ echo -e "\e[33mInstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate mongodb listen address\e[0m"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33mStart mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
