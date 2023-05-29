b3ee914090b978f1d912906e89cc5e4148bf8998
sahityareddy <sahitya.murikinati@insparx.com>
2023-05-29 16:07:18 +0200

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,10 @@ echo -e "\e[33mCopy Monogdb repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
-##TODO modify config file
+
+echo -e "\e[33mUpdate mongodb listen address\e[0m"
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+
 echo -e "\e[33mStart mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
