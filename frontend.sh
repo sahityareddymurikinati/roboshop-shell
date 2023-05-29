186a281876d29733c82ee297d1bad2604161c333
sahityareddy <sahitya.murikinati@insparx.com>
2023-05-29 16:55:07 +0200

    mongo

--- frontend.sh
+++ frontend.sh
@@ -4,14 +4,14 @@ yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[33mRemoving old app content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e33mDownloading Frontend Content\e[0m"
+echo -e "\e[33mDownloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33mExtract Frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[33mStarting Server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
