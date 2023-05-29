1f0954ffe6b01a77b02e75a06a36c22a37308038
sahityareddy <sahitya.murikinati@insparx.com>
2023-05-29 13:18:40 +0200

    >>

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,8 @@
 echo -e "\e[33mCopy Monogdb repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling mongodb server\e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 ##TODO modify config file
 echo -e "\e[33mStart mongodb service\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
