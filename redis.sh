57013c6b8936ccfcf262540f78f12880c94980d7
sahityareddy <sahitya.murikinati@insparx.com>
2023-05-30 07:52:26 +0200

    redis

--- redis.sh
+++ redis.sh
@@ -12,4 +12,4 @@ sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/robo
 
 echo -e "\e[33m enable and start redis\e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
-systemctl start redis &>>/tmp/roboshop.log
+systemctl restart redis &>>/tmp/roboshop.log
